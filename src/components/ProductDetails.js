0e60c4a132c8988e394f135ee58110f43ed110cf
AllLay <alllayisgood@gmail.com>
2025-03-20 13:19:07 +0630

    Update ProductDetails.js

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -16,25 +16,27 @@ const ProductDetail = () => {
   };
 
   return (
-    <div className="p-6 text-gray-600 flex justify-center">
-      <div className="bg-gray-800 p-6 rounded-lg shadow-lg flex items-center gap-6 w-[600px]">
+    <div className="p-4 flex justify-center items-center min-h-screen">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg flex flex-col sm:flex-row items-center sm:items-start gap-6 w-full max-w-2xl">
         <img
           src={product.image}
           alt={product.name}
-          className="w-48 h-auto rounded-md"
+          className="w-full sm:w-48 h-auto rounded-md"
         />
-        <div>
-            <h2 className="text-xl font-bold mb-2">{product.name}</h2>
-            <h3 className="mb-2">By {product.company}</h3>
-            <p className="text-lg font-bold">{new Intl.NumberFormat().format(product.price)} MMK</p>
-            <p className="mt-3">{product.description}</p>
-            <p>Contact for more Info: 09445500066</p>
-            <button
-              onClick={handleBuy}
-              className="mt-6 bg-blue-500 text-white p-3 rounded-md w-full sm:w-auto hover:bg-blue-700 transition duration-300"
-            >
-              Buy
-            </button>
+        <div className="text-center sm:text-left">
+          <h2 className="text-lg sm:text-xl font-bold mb-2">{product.name}</h2>
+          <h3 className="text-sm sm:text-base mb-2">By {product.company}</h3>
+          <p className="text-sm sm:text-lg font-bold">
+            {new Intl.NumberFormat().format(product.price)} MMK
+          </p>
+          <p className="mt-3 text-xs sm:text-sm">{product.description}</p>
+          <p className="text-xs sm:text-sm">Contact for more Info: 09445500066</p>
+          <button
+            onClick={handleBuy}
+            className="mt-6 bg-blue-500 text-white p-3 rounded-md w-full sm:w-auto hover:bg-blue-700 transition duration-300"
+          >
+            Buy
+          </button>
         </div>
       </div>
     </div>
