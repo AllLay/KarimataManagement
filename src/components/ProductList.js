3a27c0a9dbee952f5a7179f0cba4591ad2def7f0
AllLay <alllayisgood@gmail.com>
2025-03-24 19:26:07 +0630

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,19 +4,15 @@ import productsData from "./products.json";
 
 const ProductList = () => {
   const [products] = useState(productsData);
-
-  const formatPrice = (price) => {
-    return new Intl.NumberFormat().format(price);
-  };
-
+  
   return (
-    <div className="text-gray-100 p-4 flex justify-center items-center">
+    <div className="bg-black text-gray-100 p-4 flex justify-center items-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 w-full text-center">
         {products.map((product) => (
           <Link
             key={product.id}
             to={`/products/${product.id}`}
-            className="bg-gray-800 p-3 rounded-lg shadow-lg transform transition-transform duration-200 hover:scale-105 flex flex-col justify-between w-full max-w-xs mx-auto"
+            className="bg-[#2A2A2A] p-3 rounded-lg shadow-lg transform transition-transform duration-200 hover:scale-105 flex flex-col justify-between w-full max-w-xs mx-auto"
           >
             <img
               src={product.image}
@@ -27,9 +23,6 @@ const ProductList = () => {
               {product.name}
             </h3>
             <h4 className="text-xs sm:text-sm md:text-base">By {product.company}</h4>
-            <p className="text-sm sm:text-md md:text-lg font-bold">
-              {formatPrice(product.price)} MMK
-            </p>
           </Link>
         ))}
       </div>
